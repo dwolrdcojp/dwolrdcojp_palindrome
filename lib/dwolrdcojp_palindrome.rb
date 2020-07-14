baaa6335df3ea42241afe4eb65ca964903b53b05
Max Schrementi <dwolrdcojp@gmail.com>
2020-07-14 15:16:54 -0500

    Working palindrome gem

--- lib/dwolrdcojp_palindrome.rb
+++ lib/dwolrdcojp_palindrome.rb
@@ -1,6 +1,27 @@
 require "dwolrdcojp_palindrome/version"
 
-module DwolrdcojpPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters in the string.
+  def letters
+    the_letters = []
+    for i in 0..self.length - 1 do
+      if self[i].match(/[a-zA-z]/)
+      the_letters << self[i]
+      end      
+    end
+    the_letters.join
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.letters.downcase
+    end
 end
